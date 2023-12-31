3f1de90b35cd7b4917cafd75a29a3848c9a3796c
Anh Minh Do <minhdo1995@gmail.com>
2024-01-01 00:52:51 +0100

    add tests for Settings.py

---
+++ tests/test_Settings.py
@@ -0,0 +1,6 @@
+from source.Settings import Settings
+
+def test_get_setting() -> None:
+    settings_info = Settings("./tests/test_data/sample_settings_file.yaml")
+    assert settings_info.get_setting("ask_when_exit") is True
+    assert settings_info.get_setting("tem_dir") is None
